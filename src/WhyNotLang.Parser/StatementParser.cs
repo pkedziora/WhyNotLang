4db2d4874e681a5683ef676ba197c83495980ae6
Pawel Kedziora <pakedziora@gmail.com>
2019-07-18 22:31:10 +0100

    Empty statement parser class

--- src/WhyNotLang.Parser/StatementParser.cs
+++ src/WhyNotLang.Parser/StatementParser.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+using WhyNotLang.Parser.Statements;
+
 namespace WhyNotLang.Parser
 {
     public class StatementParser
     {
-        
+        private readonly ITokenIterator _tokenIterator;
+        private readonly IExpressionParser _expressionParser;
+
+        public StatementParser(ITokenIterator tokenIterator, IExpressionParser expressionParser)
+        {
+            _tokenIterator = tokenIterator;
+            _expressionParser = expressionParser;
+        }
+
+        public List<IStatement> Parse()
+        {
+            
+        }
     }
 }
