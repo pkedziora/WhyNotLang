f79c1ff4a39811fc204d44a123049790adb48ac0
Pawel Kedziora <pakedziora@gmail.com>
2019-07-20 17:51:42 +0100

    Test multiple statement types in one program

--- test/WhyNotLang.Parser.Tests/Statements/MultipleStatementsTests.cs
+++ test/WhyNotLang.Parser.Tests/Statements/MultipleStatementsTests.cs
@@ -112,5 +112,67 @@ namespace WhyNotLang.Parser.Tests.Statements
             
             Assert.Equal(expected, actual);
         }
+        
+        [Fact]
+        public void ParsesVarAssignmentAndIfAndWhileAndFunctionAndVarDeclaration()
+        {
+            _parser.Initialise(
+                @" x = (1 + 2) * 3
+                            if (x < y)
+                                x = 1
+                            else
+                                y = 2
+                            while (x < y)
+                            begin
+                                x = x + 1
+                                y = y - 2
+                            end
+                            foo(1,2)
+                            var y = (1 * (2 + 3))");
+
+            //If
+            var expectedIfCondition = TestHelpers.GetBinaryExpressionWithIdentifiers("x", "<", "y");
+            var expectedIfBody = TestHelpers.GetVariableAssignementStatement("x", TestHelpers.GetValueExpression(1));
+            var expectedElse = TestHelpers.GetVariableAssignementStatement("y", TestHelpers.GetValueExpression(2));
+            var expectedIfStatement = new IfStatement(
+                expectedIfCondition, 
+                expectedIfBody,
+                expectedElse);
+            
+            //While
+            var expectedCondition = TestHelpers.GetBinaryExpressionWithIdentifiers("x", "<", "y");
+            var expectedBody = new BlockStatement(new List<IStatement>()
+            {
+                TestHelpers.GetVariableAssignementStatement("x", TestHelpers.GetBinaryExpressionWithIdentifiers("x", "+", 1)),
+                TestHelpers.GetVariableAssignementStatement("y", TestHelpers.GetBinaryExpressionWithIdentifiers("y", "-", 2))
+            });
+            var expectedWhileStatement = new WhileStatement(
+                expectedCondition, 
+                expectedBody);
+            
+            //Function call
+            var expectedParameters = new List<IExpression>
+            {
+                new ValueExpression(new Token(TokenType.Number, "1")),
+                new ValueExpression(new Token(TokenType.Number, "2")),
+            };
+            var expectedFunctionCallStatement = new FunctionCallStatement(new FunctionExpression(new Token(TokenType.Identifier, "foo"), 
+                expectedParameters));
+            
+            var expected = new List<IStatement>
+            {
+                TestHelpers.GetVariableAssignementStatement("x",
+                    TestHelpers.GetBinaryExpression(TestHelpers.GetBinaryExpression(1, "+", 2), "*", 3)),
+                expectedIfStatement,
+                expectedWhileStatement,
+                expectedFunctionCallStatement,
+                TestHelpers.GetVariableDeclarationStatement("y",
+                    TestHelpers.GetBinaryExpression(1, "*", TestHelpers.GetBinaryExpression(2, "+", 3)))
+            };
+
+            var actual = _parser.ParseAll();
+            
+            Assert.Equal(expected, actual);
+        }
     }
 }
