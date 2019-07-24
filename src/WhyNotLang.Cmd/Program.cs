e979f7a02fea3899e02ca088f2e90565924c4247
Pawel Kedziora <pakedziora@gmail.com>
2019-07-24 20:10:05 +0100

    Support running outside debugger

--- src/WhyNotLang.Cmd/Program.cs
+++ src/WhyNotLang.Cmd/Program.cs
@@ -8,13 +8,34 @@ namespace WhyNotLang.Cmd
     {
         static void Main(string[] args)
         {
+            string fileName;
+            if(System.Diagnostics.Debugger.IsAttached)
+            {
+                fileName = "Samples/Test.wnl";
+            }
+            else
+            {
+                if (args.Length != 1)
+                {
+                    Console.WriteLine("Plese provide path to the program to execute");
+                    return;
+                }
+                
+                fileName = args[0];
+            }
+
+
             var programState = new ProgramState();
             var executor = ExecutorFactory.CreateExecutor(programState);
 
-            var program = File.ReadAllText("Samples/Test.wnl");
+            var program = File.ReadAllText(fileName);
             executor.Initialise(program);
             executor.ExecuteAll();
-            Console.ReadLine();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
