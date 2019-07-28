85743d106635d2eee832ae49a37133a2add667fc
Pawel Kedziora <pakedziora@gmail.com>
2019-07-28 17:21:47 +0100

    Blazor .NET proof of concept executing WhyNotLang interpreter

---
+++ src/WhyNotLang.Web/ServiceProviderExtensions.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using WhyNotLang.Interpreter.Builtin;
+using WhyNotLang.Interpreter.Evaluators.ExpressionValues;
+
+namespace WhyNotLang.Web
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void AddWebInputOutput(this IServiceProvider serviceProvider, Microsoft.JSInterop.IJSRuntime jsRuntime)
+        {
+            var functionCollection = serviceProvider.GetRequiredService<IBuiltinFunctionCollection>();
+            functionCollection.Add("Writeln",
+                arguments =>
+                {
+                    var str = arguments.Single();
+                    if (str.Type != ExpressionValueTypes.String)
+                    {
+                        throw new Exception("String expected");
+                    }
+
+
+                    jsRuntime.InvokeAsync<string>("console.log", str.Value);
+
+                    return ExpressionValue.Empty;
+                });
+        }
+    }
+}
---
+++ src/WhyNotLang.Web/Startup.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using WhyNotLang.Interpreter;
+
+namespace WhyNotLang.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddWhyNotLang();
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            var jsRuntime = app.Services.GetRequiredService<IJSRuntime>();
+            app.Services.AddWebInputOutput(jsRuntime);
+            app.AddComponent<App>("app");
+        }
+    }
+}
