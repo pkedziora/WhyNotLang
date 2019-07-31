357b2104e1ff022df0130ccb5faf258e616d556d
Pawel Kedziora <pakedziora@gmail.com>
2019-07-31 21:16:33 +0100

    Web built in Readln

--- src/WhyNotLang.Web/wwwroot/js/whynotlang.js
+++ src/WhyNotLang.Web/wwwroot/js/whynotlang.js
@@ -3,8 +3,14 @@
         document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight;
     }
 
+    function setFocus(id) {
+        document.getElementById(id).focus();
+        return true;
+    }
+
     return {
-        scrollOutput
+        scrollOutput,
+        setFocus
     };
 })();
 
