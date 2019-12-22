2b78c3b21b8288dd5a93dfff4d5ff4323900d32d
Pawel Kedziora <pakedziora@gmail.com>
2019-12-23 00:23:28 +0100

    Upgrade sdk

--- scripts/install-dotnet-preview.sh
+++
@@ -1,2 +0,0 @@
-cd scripts/vendor
-sudo ./dotnet-install.sh --version 3.0.100-preview8-013656 --install-dir /usr/share/dotnet/
---
+++ scripts/install-dotnet.sh
@@ -0,0 +1,2 @@
+cd scripts/vendor
+sudo ./dotnet-install.sh --version 3.1.100 --install-dir /usr/share/dotnet/
