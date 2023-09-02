97247970c160405783a524932abbc949b16350a0
Pawel Kedziora <pakedziora@gmail.com>
2023-09-02 18:24:16 +0100

    Move project location

---
+++ src/WhyNotLang.Blazor/wwwroot/js/whynotlang.canvas.js
@@ -0,0 +1,57 @@
+﻿var WhyNotLang = WhyNotLang || {};
+
+WhyNotLang.Canvas = WhyNotLang.Canvas || (function () {
+    var canvas;
+    var ctx;
+
+    function initGraphics(canvasId) {
+        canvas = document.getElementById(canvasId);
+        ctx = canvas.getContext("2d");
+    }
+
+    function clearScreen() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function drawRectangle(x, y, width, height, color) {
+        var oldFillStyle = ctx.fillStyle;
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, width, height);
+        ctx.fillStyle = oldFillStyle;
+    }
+
+    function drawText(text, x, y, color, font) {
+        var oldFillStyle = ctx.fillStyle;
+        ctx.fillStyle = color;
+        ctx.font = font;
+        ctx.fillText(text, x, y);
+        ctx.fillStyle = oldFillStyle;
+    }
+
+    function handleKeyDown(event) {
+        DotNet.invokeMethodAsync('WhyNotLang.Blazor', 'OnKeyDown', event.key)
+            .then(message => {
+            });
+    }
+
+    function handleKeyUp(event) {
+        DotNet.invokeMethodAsync('WhyNotLang.Blazor', 'OnKeyUp', event.key)
+            .then(message => {
+            });
+    }
+
+    window.onkeydown = function (args) {
+        handleKeyDown(args);
+    }
+
+    window.onkeyup = function (args) {
+        handleKeyUp(args);
+    }
+
+    return {
+        initGraphics,
+        clearScreen,
+        drawRectangle,
+        drawText
+    }
+})();
---
+++ src/WhyNotLang.Blazor/wwwroot/js/whynotlang.text.js
@@ -0,0 +1,32 @@
+﻿var WhyNotLang = WhyNotLang || {};
+
+WhyNotLang.Text = WhyNotLang.Text || (function () {
+    function scrollOutput() {
+        document.getElementById("output").scrollTop = document.getElementById("output").scrollHeight;
+    }
+
+    function setFocus(id) {
+        document.getElementById(id).focus();
+        return true;
+    }
+
+    function allowTextAreaTabs(textAreaId) {
+        var textarea = document.getElementById(textAreaId);
+
+        textarea.onkeydown = function (e) {
+            if (e.keyCode === 9 || e.which === 9) {
+                e.preventDefault();
+                var start = this.selectionStart;
+                this.value = this.value.substring(0, this.selectionStart) + "    " + this.value.substring(this.selectionEnd);
+                this.selectionEnd = start + 4;
+            }
+        };
+    }
+    
+    return {
+        scrollOutput,
+        setFocus,
+        allowTextAreaTabs
+    };
+})();
+
